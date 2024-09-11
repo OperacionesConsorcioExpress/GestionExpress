63d06cf1f5943883b99684f3cae30bb2c1519925
CEINFCEXP <cexpcinf@gmail.com>
2024-09-11 17:11:55 -0500

    Prueba de botón y publicación

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sergio.hincapie\OneDrive - Grupo Express\Gestión de la Operación\0 - Script Python\GestiónExpress\venv
+set VIRTUAL_ENV=C:\Users\wilson.sarmiento\OneDrive - Grupo Express\Escritorio\Desarrolo WEB\GestionExpress\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
